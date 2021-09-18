2402857a19802bf586ff86331291bfa69f7cdb41
gurjeet-kj <C0817568@mylambton.ca>
2021-09-19 05:21:37 +0530

    Initial Commit

---
+++ Lab_A1_A2_iOS_Gurjeet_C0817568/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab_A1_A2_iOS_Gurjeet_C0817568
+//
+//  Created by Gurjeet KJ on 19/09/21.
+//  Copyright © 2021 Gurjeet KJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
